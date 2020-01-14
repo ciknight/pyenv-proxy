b525d8d7d4d102b159382915c6645933890412fe
Andy Wang <ci_knight@msn.cn>
2020-01-14 16:55:29 +0800

    <feat>: Support tar.xz

--- src/app.py
+++ src/app.py
@@ -2,25 +2,34 @@
 # -*- coding: utf-8 -*-
 from aiohttp import web
 
-MIRROR_URL = 'https://npm.taobao.org/mirrors/python'
+MIRROR_URL = "https://npm.taobao.org/mirrors/python"
 VERSION_MAP: dict = {}
 
 
 def load_versions():
     global VERSION_MAP
-    with open('src/versions.map', 'r') as fp:
-        VERSION_MAP = {line.split(' ')[1].strip(): line.split(' ')[0] for line in fp.readlines() if line}
+    with open("src/versions.map", "r") as fp:
+        lines = [line.strip() for line in fp.readlines() if line.strip()]
+
+    for line in lines:
+        if len(line.split(" ")) == 3:
+            version, sha2name, sha2xzname = line.split(" ")
+            VERSION_MAP[sha2name] = (version, "tgz")
+            VERSION_MAP[sha2xzname] = (version, "tar.xz")
+        if len(line.split(" ")) == 2:
+            version, sha2name = line.split(" ")
+            VERSION_MAP[sha2name] = (version, "tgz")
 
 
 async def handle(request):
-    name = request.match_info.get('sha2name')
-    version = VERSION_MAP.get(name)
+    name = request.match_info.get("sha2name")
+    version, suffix = VERSION_MAP.get(name, (None, None))
     if version is None:
         raise web.HTTPNotFound()
 
-    raise web.HTTPFound(f'{MIRROR_URL}/{version}/Python-{version}.tgz')
+    raise web.HTTPFound(f"{MIRROR_URL}/{version}/Python-{version}.{suffix}")
 
 
 load_versions()
 app = web.Application()
-app.add_routes([web.get('/{sha2name}', handle)])
+app.add_routes([web.get("/{sha2name}", handle)])
--- update.sh
+++ update.sh
@@ -5,8 +5,13 @@ for file in pyenv/plugins/python-build/share/python-build/*; do
   if [ -f "$file" ]; then
     basename="$(basename "$file")"
     sha2name="$(cat $file | grep 'Python-' | grep 'tgz' | awk -F '#' '{print $2}' | awk -F '"' '{print $1}')"
+    sha2xzname="$(cat $file | grep 'Python-' | grep 'tar.xz' | awk -F '#' '{print $2}' | awk -F '"' '{print $1}')"
     if [ -n "$sha2name" ]; then
-      echo $basename $sha2name >> src/versions.map
+       if [ -n "$sha2xzname" ]; then
+         echo $basename $sha2name $sha2xzname >> src/versions.map
+       else
+         echo $basename $sha2name >> src/versions.map
+       fi
     fi
   fi
 done
