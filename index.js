6bfdaf0005aeccf51ec396c3846d4aa6cdfb126c
Andy Wang <ci_knight@msn.cn>
2021-01-31 14:45:52 +0800

    feat: update index.js

--- index.js
+++ index.js
@@ -8,8 +8,19 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+function simpleResponse(statusCode, message) {
+  let resp = {
+    message: message,
+    status: statusCode
+  }
+
+  return new Response(JSON.stringify(resp), {
+    headers: { "Content-Type": "application/json" },
+    status: statusCode
+  })
+}
+
 MIRROR_URL = "https://cdn.npm.taobao.org/dist/python"
-statusCode = 301
 
 /**
  * Respond to the request
@@ -19,7 +30,11 @@ async function handleRequest(request) {
   const url = new URL(request.url)
   sha2name = url.pathname.replace("/", "")
   config = JSON.parse(await PYENV_PROXY.get("config"))
-  version = config[sha2name][0]
-  suffix = config[sha2name][1]
-  return Response.redirect(MIRROR_URL + "/" + version + "/Python-" + version + "." + suffix, statusCode)
+  if(config[sha2name]) {
+    version = config[sha2name][0]
+    suffix = config[sha2name][1]
+    return Response.redirect(MIRROR_URL + "/" + version + "/Python-" + version + "." + suffix, 301) 
+  }
+
+  return simpleResponse(404, "Not Found.")
 }
