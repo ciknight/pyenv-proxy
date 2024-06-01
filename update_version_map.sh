8c273a23477f4fc044d468af814b2a9faa8ae034
Andy Wang <ci_knight@msn.cn>
2024-06-01 11:08:48 +0800

    feat: update python version map

--- update_version_map.sh
+++ update_version_map.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo > src/versions.map
+echo > versions.map
 for file in pyenv/plugins/python-build/share/python-build/*; do
   if [ -f "$file" ]; then
     basename="$(basename "$file")"
@@ -8,9 +8,9 @@ for file in pyenv/plugins/python-build/share/python-build/*; do
     sha2xzname="$(cat $file | grep 'Python-' | grep 'tar.xz' | awk -F '#' '{print $2}' | awk -F '"' '{print $1}')"
     if [ -n "$sha2name" ]; then
        if [ -n "$sha2xzname" ]; then
-         echo $basename $sha2name $sha2xzname >> src/versions.map
+         echo $basename $sha2name $sha2xzname >> versions.map
        else
-         echo $basename $sha2name >> src/versions.map
+         echo $basename $sha2name >> versions.map
        fi
     fi
   fi
